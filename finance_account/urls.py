3b8b1980fb10a8759eebdda03d12e6f8a492db0a
Vinny <vinnyshipley@gmail.com>
2023-08-10 04:06:29 +0000

    finally displaying the html template. I am become hack, the destroyer of decent code

--- finance_account/urls.py
+++ finance_account/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UserListView, UserDetail
 
 urlpatterns = [
-  path('', UserListView.as_view(), name='user_list.html'),
+  path('', UserListView.as_view(), name='user_list'),
   path('accounts/<int:pk>', UserDetail.as_view(), name='user_detail')
 ]
--- finance_account/views.py
+++ finance_account/views.py
@@ -1,16 +1,29 @@
-from .models import FinanceAccount
-from .serializers import AccountSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+# from .models import FinanceAccount
+# from .serializers import AccountSerializer
+# from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 
-class UserListView(ListCreateAPIView):
-  template_name = 'templates/user_list.html'
-  queryset = FinanceAccount.objects.all()
-  serializer_class = AccountSerializer
-  context_object_name = 'accounts'
+# class UserListView(ListCreateAPIView):
+#   template_name = 'templates/user_list.html'
+#   queryset = FinanceAccount.objects.all()
+#   serializer_class = AccountSerializer
+#   context_object_name = 'accounts'
 
   
-class UserDetail(RetrieveDestroyAPIView):
-  queryset = FinanceAccount.objects.all()
-  serializer_class = AccountSerializer
-  context_object_name = 'accounts'
+# class UserDetail(RetrieveDestroyAPIView):
+#   queryset = FinanceAccount.objects.all()
+#   serializer_class = AccountSerializer
+#   context_object_name = 'accounts'
+
+from django.views.generic import ListView, DetailView
+from .models import FinanceAccount
+
+class UserListView(ListView):
+    template_name = 'user_list.html'
+    queryset = FinanceAccount.objects.all()
+    context_object_name = 'accounts'
+
+class UserDetail(DetailView):
+    template_name = 'user_detail.html'
+    queryset = FinanceAccount.objects.all()
+    context_object_name = 'account'
 
