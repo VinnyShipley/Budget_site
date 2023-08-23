decc0588e014c31516c40b6eb44fbc11267576b4
Vinny <vinnyshipley@gmail.com>
2023-08-23 03:04:15 +0000

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Couple Budgeting Site
+
+## An easy way to split finances and expenses between two or more people differing incomes. Also able to calculate what percentage each person should contribute based on their incomes
--- finance_account/views.py
+++ finance_account/views.py
@@ -4,6 +4,7 @@ from .forms import UserInfoForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 import json
+from django.contrib.auth import get_user_model
 
 class UserListView(ListView):
     template_name = 'user_list.html'
@@ -17,16 +18,23 @@ class UserDetail(DetailView):
 
 def new(request):
     if request.method == 'POST':
-        form = UserInfoForm(request.POST)
-        if form.is_valid():
-            # Serialize the list into a string
-            income_names = json.dumps(form.cleaned_data['income_names'])
-            
-            finance_account = form.save(commit=False)
-            finance_account.income_names = income_names
-            finance_account.save()
-            
-            return redirect('success')  # Redirect to a success page or another view
+        income_names = request.POST.getlist('income_names')  # Replace with the actual field name
+        incomes = request.POST.getlist('incomes')  # Replace with the actual field name
+
+        income_names_str = '{{{}}}'.format(', '.join('"{}"'.format(item) for item in income_names))
+        incomes_str = '{{{}}}'.format(', '.join(str(item) for item in incomes))
+
+        user = get_user_model().objects.get(username=request.user.username)
+
+        finance_account = FinanceAccount(
+            owner=user,
+            income_names=income_names_str,
+            incomes=incomes_str,
+            # other fields...
+        )
+        finance_account.save()
+
+        # Redirect or render success page here
     else:
-        form = UserInfoForm()
-    return render(request, 'form.html', {'form': form})
+        # Handle GET request
+        return render(request, 'your_template.html', context)
