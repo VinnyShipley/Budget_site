44ece535ede4537db207db5c70346a47c8ac2211
Vinny <vinnyshipley@gmail.com>
2023-09-17 20:52:30 +0000

    Added percentage to the table

--- finance_account/budget_splitter.py
+++ finance_account/budget_splitter.py
@@ -3,13 +3,13 @@ def budget_splitter(names, incomes, expenses):
     if combined_spending_money < 0:
         return "Total expenses exceed total income. Adjust your expenses."
 
-    num_people = len(names)
     spending_percentages = [income / sum(incomes) for income in incomes]
     spending_amounts = [combined_spending_money * percentage for percentage in spending_percentages]
 
     results = []
     for name, income, expense, spending_amount in zip(names, incomes, expenses, spending_amounts):
         remaining_income = income - expense
+        personal_percentages = (remaining_income / sum([income - expense for income, expense in zip(incomes, expenses)])) * 100
         individual_spending = remaining_income + spending_amount
         results.append({
             'name': name,
@@ -18,6 +18,7 @@ def budget_splitter(names, incomes, expenses):
             'spending_amount': spending_amount,
             'remaining_income': remaining_income,
             'individual_spending': individual_spending,
+            'personal_percentage': personal_percentages,
         })
     print("Intermediate Results:")
     for result in results:
