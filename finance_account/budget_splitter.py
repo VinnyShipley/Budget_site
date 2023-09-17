3bc6d3cae1d385a5823fe220dd012fe39570fb49
Vinny <vinnyshipley@gmail.com>
2023-09-17 20:55:35 +0000

    Got rid of unneeded columns

--- finance_account/budget_splitter.py
+++ finance_account/budget_splitter.py
@@ -7,17 +7,14 @@ def budget_splitter(names, incomes, expenses):
     spending_amounts = [combined_spending_money * percentage for percentage in spending_percentages]
 
     results = []
-    for name, income, expense, spending_amount in zip(names, incomes, expenses, spending_amounts):
+    for name, income, expense in zip(names, incomes, expenses):
         remaining_income = income - expense
         personal_percentages = (remaining_income / sum([income - expense for income, expense in zip(incomes, expenses)])) * 100
-        individual_spending = remaining_income + spending_amount
         results.append({
             'name': name,
             'income': income,
             'expense': expense,
-            'spending_amount': spending_amount,
             'remaining_income': remaining_income,
-            'individual_spending': individual_spending,
             'personal_percentage': personal_percentages,
         })
     print("Intermediate Results:")
